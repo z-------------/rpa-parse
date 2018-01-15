24c1b9f75c234333b6819d8b7f9063df95125caa
Zachary Guard <zacharyguard@gmail.com>
2018-01-15 18:51:36 +0800

    Option to automatically add file extensions

--- bin/main.js
+++ bin/main.js
@@ -3,26 +3,30 @@
 const path = require("path");
 const parseRPA = require("../lib/parse-rpa.js");
 
-if (process.argv.length === 2 || process.argv[2] === "-h" || process.argv[2] === "--help") {
-  console.log("Usage: rpa-parse <filename> [<output dir>]");
-} else {
+let args = require("minimist")(process.argv.slice(2));
+
+let usageString = "Usage: rpa-parse <filename> [<output dir>] [--auto-ext]";
+
+if (args._.length === 0 || args.h || args.help) {
+  console.log(usageString);
+} else if (args._.length >= 1) {
   var targetFileName, outputDirname;
 
-  if (path.isAbsolute(process.argv[2])) {
-    targetFileName = process.argv[2];
+  if (path.isAbsolute(args._[0])) {
+    targetFileName = args._[0];
   } else {
-    targetFileName = path.join(process.cwd(), process.argv[2])
+    targetFileName = path.join(process.cwd(), args._[0])
   }
 
-  if (process.argv[3]) {
-    if (path.isAbsolute(process.argv[3])) {
-      outputDirname = process.argv[3];
+  if (args._[1]) {
+    if (path.isAbsolute(args._[1])) {
+      outputDirname = args._[1];
     } else {
-      outputDirname = path.join(process.cwd(), process.argv[3])
+      outputDirname = path.join(process.cwd(), args._[1])
     }
   } else {
     outputDirname = process.cwd();
   }
 
-  parseRPA(targetFileName, outputDirname);
+  parseRPA(targetFileName, outputDirname, !!args["auto-ext"]);
 }
